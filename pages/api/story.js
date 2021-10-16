a5b9be2d859e656835983ca0b923ded2f3f3de30
IgorRoc <igor_roc@hotmail.com.br>
2021-10-16 13:10:10 -0300

    [test]: Get dirpath

--- pages/api/story.js
+++ pages/api/story.js
@@ -3,12 +3,22 @@ var fs = require("fs")
 function Story(request, response) {
 	var options = request.query
 
-	// var jsonDB = getDB("stories.json")
+	var jsonDB = getDB("stories.json")
 
-	// let max = jsonDB.length
-	// let random = Math.floor(Math.random() * (max + 1))
+	let max = 0
+	if(jsonDB){
+		max = jsonDB.length
+	}
+	let random = Math.floor(Math.random() * (max + 1))
 
 	response.json(options)
+}
+
+function getDB(file) {
+	console.log(__dirname)
+	// var firstPath = __dirname.split("\\dark-stories")[0]
+	// var filepath = firstPath + "\\dark-stories\\" + file
+	// return readJsonFileSync(filepath)
 }
 
 function readJsonFileSync(filepath, encoding) {
@@ -19,10 +29,5 @@ function readJsonFileSync(filepath, encoding) {
 	return JSON.parse(file)
 }
 
-function getDB(file) {
-	var firstPath = __dirname.split("\\dark-stories")[0]
-	var filepath = firstPath + "\\dark-stories\\" + file
-	return readJsonFileSync(filepath)
-}
 
 export default Story
