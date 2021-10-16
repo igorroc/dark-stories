d94e4e69f830b593221cb373f8e45b902a9b2c03
IgorRoc <igor_roc@hotmail.com.br>
2021-10-16 11:54:23 -0300

    [del]: Out folder

--- out/_next/static/Lkp3_OmKJIQdg7D_WbI0w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-de54d27be2f26032eac5.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
